39f9d61fd7d999414d67116f31d0630165473a2a
ningyixiao <zjutsgg@gmail.com>
2017-04-20 21:52:09 +0800

    the i18n config of contact page

--- v1/pages/js/contact.js
+++ v1/pages/js/contact.js
@@ -4,7 +4,28 @@ var loadPath_en = "/locales/en/en-contact.json";
 // 提供给百度地图api的变量，需要先初始化
 var company = "我们公司";
 var map_address = "香港中环荆威广场17楼";
+
+function callBaiduMapApi(company,map_address) {
+    // 百度地图API功能
+    var map = new BMap.Map("map-container");
+    var point = new BMap.Point(114.167791, 22.284156);
+    var marker = new BMap.Marker(point); // 创建标注
+    map.addOverlay(marker); // 将标注添加到地图中
+    map.centerAndZoom(point, 16); //15指地图的缩放程度
+    var opts = {
+        width: 200, // 信息窗口宽度
+        height: 100, // 信息窗口高度
+        title: company, // 信息窗口标题
+        enableMessage: true, //设置允许信息窗发送短息
+        message: ""
+    }
+    var infoWindow = new BMap.InfoWindow(map_address, opts); // 创建信息窗口对象 
+    marker.addEventListener("click", function() {
+        map.openInfoWindow(infoWindow, point); //开启信息窗口
+    });
+}
 $(function() {
+    callBaiduMapApi(company,map_address);
     $("#choose_lang a").click(function(e) {
         e.preventDefault();
         var choose_lang = $(e.target).attr("href");
@@ -31,6 +52,7 @@ $(function() {
                     company = "我们公司";
                     map_address = "香港中环荆威广场17楼";
             }
+            callBaiduMapApi(company,map_address);
             // console.log(load_path)
             i18next.use(i18nextXHRBackend).init({
                 backend: {
@@ -48,4 +70,3 @@ $(function() {
         }
     });
 });
-
