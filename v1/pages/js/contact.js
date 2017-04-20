36b3cac4605412ccbfa8c32280edaacf03fe54de
ningyixiao <zjutsgg@gmail.com>
2017-04-20 21:43:48 +0800

    the i18n config of contact page

--- v1/pages/js/contact.js
+++ v1/pages/js/contact.js
@@ -1,6 +1,9 @@
 var lang = "zh";
 var loadPath_zh = "/locales/zh/zh-contact.json";
 var loadPath_en = "/locales/en/en-contact.json";
+// 提供给百度地图api的变量，需要先初始化
+var company = "我们公司";
+var map_address = "香港中环荆威广场17楼";
 $(function() {
     $("#choose_lang a").click(function(e) {
         e.preventDefault();
@@ -13,14 +16,20 @@ $(function() {
                 case "en":
                     load_path = loadPath_en;
                     lang = "en";
+                    company = "my company";
+                    map_address = "17/F, Silver Fortune Plaza, No.1 Wellington Street, Central";
                     break;
                 case "zh":
                     load_path = loadPath_zh;
                     lang = "zh";
+                    company = "我们公司";
+                    map_address = "香港中环荆威广场17楼";
                     break;
                 default:
                     load_path = loadPath_zh;
                     lang = "zh";
+                    company = "我们公司";
+                    map_address = "香港中环荆威广场17楼";
             }
             // console.log(load_path)
             i18next.use(i18nextXHRBackend).init({
@@ -39,19 +48,4 @@ $(function() {
         }
     });
 });
-// 提供给百度地图api的变量
-var company = "";
-var map_address = "";
-switch (lang) {
-    case "zh":
-        company = "我们公司";
-        map_address = "香港中环荆威广场17楼";
-        break;
-    case "en":
-        company = "my company";
-        map_address = "17/F, Silver Fortune Plaza, No.1 Wellington Street, Central";
-        break;
-    default:
-        company = "我们公司";
-        map_address = "香港中环荆威广场17楼";
-}
+
